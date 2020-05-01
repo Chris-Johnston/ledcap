8c2234551ce8749b80d69f7b081a129fafad943f
Chris Johnston <chris@thejohnstons.net>
2020-04-30 23:47:44 -0700

    initial commit of single strip controller

---
+++ singlestrip.py
@@ -0,0 +1,123 @@
+"""
+Single Strip LED Driver
+
+This is for showing the single strip of LEDs that I have mounted along my wall.
+"""
+
+"""
+Led Controller
+"""
+
+from controller import Controller
+from patternmanager import PatternManager
+import RPi.GPIO as GPIO
+import time
+from view import View
+import rpi_ws281x as ws
+
+LED_DIMENSIONS = (100, 1)
+
+BUTTON_NEXT = 21
+BUTTON_PREV = 12
+# dunno exactly what I'll use these extra buttons for
+BUTTON_HOLD = 25
+BUTTON_OFF = 24
+BUTTON_ON = 23
+BOUNCE_TIME = 200
+BRIGHTNESS = 250 # power supply goes brrrrr
+UPDATE_RATE = 1.0 / 100.0
+
+class SingleStripController(Controller):
+    def __init__(self, pm):
+        super().__init__(pm)
+        self.holding = False
+        self.off_index = 0
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(BUTTON_NEXT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(BUTTON_PREV, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(BUTTON_ON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(BUTTON_OFF, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(BUTTON_HOLD, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        # looks like this doesn't break, despite using interrupts
+        GPIO.add_event_detect(BUTTON_NEXT, GPIO.FALLING, callback=self.next_callback, bouncetime=BOUNCE_TIME)
+        GPIO.add_event_detect(BUTTON_PREV, GPIO.FALLING, callback=self.prev_callback, bouncetime=BOUNCE_TIME)
+        GPIO.add_event_detect(BUTTON_ON, GPIO.FALLING, callback=self.on_callback, bouncetime=BOUNCE_TIME)
+        GPIO.add_event_detect(BUTTON_OFF, GPIO.FALLING, callback=self.off_callback, bouncetime=BOUNCE_TIME)
+        GPIO.add_event_detect(BUTTON_HOLD, GPIO.FALLING, callback=self.hold_callback, bouncetime=BOUNCE_TIME)
+
+    def off_callback(self, event):
+        if not self.holding and not self.pattern_manager.is_off():
+            self.off_index = self.pattern_manager.handler_index
+            self.pattern_manager.off()
+
+    def on_callback(self, event):
+        if not self.holding and self.pattern_manager.is_off():
+            self.pattern_manager.set_index(self.off_index)
+
+    def hold_callback(self, event):
+        start = time.time()
+        while GPIO.input(BUTTON_HOLD) == GPIO.LOW:
+            time.sleep(0.01)
+        delta = time.time() - start
+        if delta > 1:
+            self.holding = not self.holding
+            print('holding: ', self.holding)
+
+    def prev_callback(self, event):
+        if not self.holding:
+            self.pattern_manager.prev_pattern()
+
+    def next_callback(self, event):
+        if not self.holding:
+            self.pattern_manager.next_pattern()
+
+class SingleStripView(View):
+    def __init__(self, colors: list, dimensions: tuple, update):
+        super().__init__(colors, dimensions, update)
+        self.count = self.dimensions[0] * self.dimensions[1]
+
+    def coord_to_strip_index(self, x: int, y: int) -> int:
+        # ignore Y
+        return x % self.dimensions[0]
+
+    def internal_draw(self, strip):
+        for x in range(self.dimensions[0]):
+            for y in range(self.dimensions[1]):
+                index = self.coord_to_strip_index(x, y)
+                strip.setPixelColor(index, self.colors[x][y])
+
+    def draw(self):
+        """
+        Draw the leds in a loop, blocks
+        """
+        strip = ws.Adafruit_NeoPixel(self.count, 18, 800000, 10, False, BRIGHTNESS)
+        strip.begin()
+        try:
+            while True:
+                self.update()
+                self.internal_draw(strip)
+                strip.show()
+                time.sleep(UPDATE_RATE
+            )
+        except KeyboardInterrupt:
+            for x in range(self.count):
+                strip.setPixelColor(x, 0)
+            strip.show()
+            time.sleep(1.0 / 50.0)
+
+def main():
+    colors = []
+    for row in range(LED_DIMENSIONS[0]):
+        new = []
+        for col in range(LED_DIMENSIONS[1]):
+            new.append(0xff00ff)
+        colors.append(new)
+
+    pm = PatternManager(colors, LED_DIMENSIONS)
+    lv = SingleStripView(colors, LED_DIMENSIONS, pm.update)
+    lc = SingleStripController(pm)
+    lv.draw()
+
+if __name__ == "__main__":
+    main()
