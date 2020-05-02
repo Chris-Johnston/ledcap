82fa7c6dee32569ae769eb43fdbe67b8a08a87ce
Chris Johnston <chris@thejohnstons.net>
2020-05-01 21:18:38 -0700

    update red pattern

--- pattern/redpattern.py
+++ pattern/redpattern.py
@@ -4,6 +4,7 @@ Red pattern
 
 from pattern.pattern import Pattern
 import time
+import random
 
 class RedPattern(Pattern):
     def __init__(self, colors: list, dimensions: tuple):
@@ -11,4 +12,10 @@ class RedPattern(Pattern):
 
     def update(self):
         for idx, val in self:
-            self[idx] = 0xff0000
+            if random.randint(0, 90) == 0:
+                self[idx] = 0xff0000
+            else:
+                r = int(0xff * math.cos((offset) / size))
+                if r < 0:
+                    r = 0
+                self[idx] = r << 16
