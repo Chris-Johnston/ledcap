ec972c153201ed6bf9adcca84783ab8ea1858eb0
Chris Johnston <chris@thejohnstons.net>
2020-05-01 23:51:20 -0700

    extend

--- pattern/hackscroller.py
+++ pattern/hackscroller.py
@@ -17,10 +17,10 @@ class HackScrollerPattern(Pattern):
         for idx, val in self:
             if idx == (self.tick % self.__max_index__()):
                 self[idx] = 0xffffff
-            elif (self.tick - 5 % self.__max_index__()) < idx < (self.tick % self.__max_index__()):
+            elif (self.tick - 10 % self.__max_index__()) < idx < (self.tick % self.__max_index__()):
                 self[idx] = 0x00ff00
             else:
-                self[idx] = 0
+                self[idx] = 0x001100
 
 
 
