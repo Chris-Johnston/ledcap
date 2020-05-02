2da75af372b8f3df1a81b5984c24dd1866b38996
Chris Johnston <chris@thejohnstons.net>
2020-05-01 21:27:09 -0700

    add other colors

--- pattern/redpattern.py
+++ pattern/redpattern.py
@@ -19,7 +19,12 @@ class RedPattern(Pattern):
                 coords = self.index_to_coords(idx)
                 size = 5.0
                 offset = time.time() * 1000.0 / 555.0
+
+                gb = int(0x55 * math.cos((coords[1] - (7.0 * offset)) / (size * 3.0)))
+                if gb < 0:
+                    gb = 0
+
                 r = 127 + int(127 * math.cos((coords[1] - offset) / size))
                 if r < 0:
                     r = 0
-                self[idx] = r << 16
+                self[idx] = r << 16 | gb << 8 | gb
