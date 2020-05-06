41f71ac01054e34d235d6199008d3e4a9a443233
Chris Johnston <chris@thejohnstons.net>
2020-05-06 00:33:29 -0700

    cos v

--- pattern/uhhhpattern.py
+++ pattern/uhhhpattern.py
@@ -19,7 +19,8 @@ class HackScrollerPattern(Pattern):
             _, y = self.index_to_coords(idx)
             dist = (y - (self.dimensions[1] / 2)) / (self.dimensions[1] / 2)
 
-            r, g, b = colorsys.hsv_to_rgb(dist % 1.0, 1.0, 1.0)
+            v = math.cos(dist) % 1.0
+            r, g, b = colorsys.hsv_to_rgb(dist % 1.0, 1.0, v)
             on_color = self.rgb_to_val(r * 255, g * 255, b * 255)
             self[idx] = on_color
             # bitmask = 1 << bit
