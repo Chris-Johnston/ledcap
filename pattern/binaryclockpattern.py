a9d7cb5fe0ee93c682cce2a0d39d7af9c5d06c65
Chris Johnston <chris@thejohnstons.net>
2020-05-05 23:59:47 -0700

    add a binary clock

---
+++ pattern/binaryclockpattern.py
@@ -0,0 +1,24 @@
+"""
+binary clock
+"""
+
+from pattern.pattern import Pattern
+import time
+import random
+import math
+
+class HackScrollerPattern(Pattern):
+    def __init__(self, colors: list, dimensions: tuple):
+        super().__init__(colors, dimensions)
+        self.on_color = 0x00ff00
+
+    def update(self):
+        time = time.time()
+        # cool idea, maybe have "on" color adjust hue over time
+        for idx, val in self:
+            bit = idx % 32
+            bitmask = 1 << bit
+            if time & bitmask > 0:
+                self[idx] = self.on_color
+            else:
+                self[idx] = 0
