9d59234c529a6e22c8036324754952a7eab35d6f
Chris Johnston <chris@thejohnstons.net>
2020-05-06 00:38:13 -0700

    idk

--- pattern/uhhhpattern.py
+++ pattern/uhhhpattern.py
@@ -19,8 +19,8 @@ class HackScrollerPattern(Pattern):
             _, y = self.index_to_coords(idx)
             dist = (y - (self.dimensions[1] / 2)) / (self.dimensions[1] / 2)
 
-            width = 0.7 * dist # math.sin()
-            shift = time.time() / 100.0
+            shift = time.time() / 2.0
+            width = 0.7 * dist + math.sin(shift)
             v = abs(math.cos(shift + dist * math.pi * width))
             r, g, b = colorsys.hsv_to_rgb(dist % 1.0, 1.0, v)
             on_color = self.rgb_to_val(r * 255, g * 255, b * 255)
